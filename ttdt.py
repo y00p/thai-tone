6dd027c5ef139a1c74f1882cc40772c6be81a29f
Julia Portl @thinkerbell <julia.portl@iwr.uni-heidelberg.de>
2017-04-28 20:25:32 +0700

    added demo mode

--- ttdt.py
+++ ttdt.py
@@ -3,7 +3,20 @@ from thaianalysis import determine_tone
 
 ts.load()
 
-# input of syllable #####################################################
-syllable = input("Please enter a Thai syllable: ")
-tone = determine_tone( syllable, verbose=False )
-print('\nThe syllable is spoken in {} tone.\n'.format(tone))
+syllable = input("Please enter a Thai syllable (or just hit Enter): ")
+
+if syllable:
+    # Process user input
+    tone = determine_tone( syllable, verbose=True )
+    print('\nThe syllable is spoken in {} tone.\n'.format(tone))
+else:
+    # Run demo data
+    ts.load(basics=False, test_cases=True)
+    for word, word_tone in zip(ts.consonant_words, ts.consonant_words_tones):
+        if len(word_tone) == 1: # only monosyllabic words
+            tone = determine_tone( word, verbose=False )
+            print('{}: {}'.format(word, tone))
+
+
+
+
